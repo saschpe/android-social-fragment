c100fe0e104df0eb0176ebf3ec26fee430a3a134
Toru Choi <blueocean239@gmail.com>
2017-10-11 01:08:50 +0800

    removing findViewById, replacing it with DataBinding Library

---
+++ social-fragment/src/main/java/saschpe/android/socialfragment/app/Const.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2017 Sascha Peilicke
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package saschpe.android.socialfragment.app;
+
+/**
+ * Created by wonyoung on 2017. 10. 11..
+ */
+
+public interface Const {
+    String ARG_APPLICATION_ID = "app_id";
+    String ARG_APPLICATION_NAME = "app_name";
+    String ARG_CONTACT_EMAIL_ADDRESS = "contact_email_address";
+    String ARG_CONTACT_EMAIL_SUBJECT = "contact_email_subject";
+    String ARG_CONTACT_EMAIL_TEXT = "contact_email_text";
+    String ARG_FACEBOOK_PAGE = "facebook_page";
+    String ARG_GOOGLE_PLUS_GROUP = "google_plus_group";
+    String ARG_ICON_TINT = "drawable_tint";
+    String ARG_RECOMMENDATION_SUBJECT = "recommendation_subject";
+    String ARG_TWITTER_PROFILE = "twitter_profile";
+    String ARG_HEADER_TEXT_COLOR = "header_text_color";
+    String ARG_GITHUB_PROJECT = "github_project";
+}
